4f5125bf8cd84d6c99d9c30608244710d272f916
Ricardo Esquivel <rickyaevarg@gmail.com>
2022-11-18 10:49:38 -0800

    Switched up logic control and added comments

--- imageTrendAuto/readMail.py
+++ imageTrendAuto/readMail.py
@@ -9,8 +9,8 @@ import re
 def main():
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI") # Open up the outlook application and its properties
     inbox = outlook.GetDefaultFolder(6)                                            # Open up the inbox folder (6)
-    messages = inbox.Items
-    filesExplored = []
+    messages = inbox.Items                                                         # Set a var to environment
+    filesExplored = []                                                             # List of files we explored
     outputDir = r"C:/Users/esquivr/Desktop/Apps/imageTrendAuto/csvFIles"
     try:
         for message in list(messages): # Go through the messages in our inbox
@@ -18,29 +18,31 @@ def main():
             if re.search("Scheduled Report", string): # Search the subject line for a report
                 if message.SenderEmailAddress == "noReply@imagetrend.com": # Check if flagged email is actually IMAGE TREND
                     try:
-                        for attachment in message.Attachments:
-                            if attachment not in filesExplored:
-                                # Record file already explored
-                                filesExplored.append(attachment)
-                                # Current file time
-                                fileTime = str(message.SentOn)
-                                fileTime = fileTime.split()[0]
-                                # Get current time
-                                currentTime = date.today()
-                                # Get one week span form today
-                                lastweekTime = datetime.today() - timedelta(days=2)
-                                lastweekTime = str(lastweekTime)
-                                lastweekTime = lastweekTime.split()[0]
-                                # We only want files in the newest span cycle
-                                if fileTime >= lastweekTime:
-                                    print(f"Email: {message.subject}")
-                                    print(f"File attachment: {attachment}")
+                        # Current file time
+                        fileTime = str(message.SentOn)
+                        fileTime = fileTime.split()[0]
+                        # Get one week span form today
+                        lastweekTime = datetime.today() - timedelta(days=2)
+                        lastweekTime = str(lastweekTime)
+                        lastweekTime = lastweekTime.split()[0]
+                        # We only want files in the newest span cycle
+                        if fileTime >= lastweekTime:
+                            print(f"Email: {message.subject}")
+                            for attachment in message.Attachments:
+                                if attachment not in filesExplored:
+                                    # Record file already explored
+                                    filesExplored.append(attachment)
                                     try:
+                                        print(f"File attachment: {attachment}\n")
+                                        print("Adding file to directory...\n")
+                                        # Add attachment to our directory
                                         attachment.SaveAsFile(os.path.join(outputDir, attachment.FileName))
                                     except Exception as error:
                                         print("error when saving the attachment:" + str(error))
-                            else:
-                                continue
+                                else:
+                                    continue
+                        #else:
+                            #continue
                     except Exception as e:
                         print("error when processing emails messages:" + str(error))
     except Exception as e:
