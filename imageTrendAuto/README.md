d2eeea5bf6cdbae0961f24987ef3795c35c8a822
Ricardo Esquivel <rickyaevarg@gmail.com>
2023-01-11 10:46:10 -0800

    Working version of python script with updated readme

--- imageTrendAuto/README.md
+++ imageTrendAuto/README.md
@@ -1,13 +1,23 @@
 # HSA Auto Report Reader
 
+Python script to save files found within emails.
+
 ## Description:
 
+This is a python script to easily retrive ImageTrend CSV file reports. Working 
+with the win32 library, I utilize this library to rapidly go through an outlook
+account and find information found within Image Trend reports to accurately 
+pull CSV files.
 
 ## Usage:
 	
+Run the exe and answer the command prompt guide. Using the exe is an easy way
+to run/managed the program to your liking.
 
-## Notes:
+## Developer Notes:
+
+Hidden import win32timezone must be include in exe transfer
 
 
 ## Python to EXE Tool	
-	$ python -m auto_py_to_exe
+$ python -m auto_py_to_exe
--- imageTrendAuto/readMail.py
+++ imageTrendAuto/readMail.py
@@ -1,4 +1,3 @@
-import promptlib
 from tkinter import Tk
 from tkinter import filedialog
 import win32com
@@ -6,6 +5,7 @@ from win32com.client import constants
 from win32com.client.gencache import EnsureDispatch as Dispatch
 import os
 from datetime import datetime, timedelta
+import time
 import re
 
 def main():
@@ -15,7 +15,7 @@ def main():
     #########################################################
     range_date_days = 0
     range_date_weeks = 0
-    range_date = str(input("How far back should I look, in weeks or days? Type W for weeks or D for days: "))
+    range_date = str(input("How far back should I look, in weeks(W) or days(D)? : "))
     if range_date == "W" or range_date == "w":
         range_date_weeks = int(input("How many weeks? : "))
     elif range_date == "D" or range == "d":
@@ -29,7 +29,7 @@ def main():
     #########################################################
     root = Tk()
     root.withdraw()
-    print("opening file dialog")
+    print("Please select a folder to hold your CSV files.")
     path = filedialog.askdirectory(title='Select Folder to Place CSV Files') # shows dialog box and return the path
     
     if path == "":
@@ -39,45 +39,43 @@ def main():
     #########################################################
     # Information dump
     ######################################################### 
-    print("Information dump!:")
-    print(f"Path user chose is {path}")
+    print("\nInformation given:")
+    print(f"Directory you chose is {path}")
     if range_date_days == 0:
-        print(f"Range is in weeks and the number is {range_date_weeks}")
+        print(f"Your range is in weeks and the number is {range_date_weeks}")
         target_date = range_date_weeks
     else:
-        print(f"Range is in days and the number is {range_date_days}")
+        print(f"Your range is in days and the number is {range_date_days}")
         target_date = range_date_days 
-    todays_date = datetime.today()
+    todays_date = datetime.today().date() 
     if range_date_days == 0:
-        restricted_date = datetime.today() - timedelta(weeks=range_date_weeks)
+        restricted_date = datetime.today().date() - timedelta(weeks=range_date_weeks)
     else:
-        restricted_date = datetime.today() - timedelta(days=range_date_days)
-    print(todays_date)
-    print(restricted_date)
-    return
+        restricted_date = datetime.today().date() - timedelta(days=range_date_days)
+    print("End of information given \n")
+    time.sleep(4)
     #########################################################
     # Open Outlook application and go through emails
     #########################################################
     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI") # Open up the outlook application and its properties
     inbox = outlook.GetDefaultFolder(6)                                            # Open up the inbox folder (6)
     messages = inbox.Items                                                         # Set a var to environment
-    messages.Sort("[ReceivedTime]", True)
+    messages.Sort("[ReceivedTime]", True)                                          # Limit time of exploring emails by restricting date
     lastRangeMessages = messages.Restrict("[ReceivedTime] >= '" +restricted_date.strftime('%m/%d/%Y %H:%M %p')+"'")
     filesExplored = []                                                             # List of files we explored
     outputDir = path
-    outOfRange = 0
 
-    print("Opening Outlook")
+    print("Opening Outlook...")
 
     try:
+        print("Reading messages...\n")
         for message in lastRangeMessages: # Go through the messages in our inbox
             string = message.subject  # Get the subject line of a email
             if re.search("Scheduled Report", string): # Search the subject line for a report
                 if message.SenderEmailAddress == "noReply@imagetrend.com": # Check if flagged email is actually IMAGE TREND
-                    print("Found report")
+                    print("Found a report")
                     try:
-                        print(message.SentOn)
-                        print(f"Email: {message.subject}")
+                        print(f"Email found: {message.subject}")
                         for attachment in message.Attachments:
                             if attachment not in filesExplored:
                                 # Record file already explored
@@ -89,19 +87,22 @@ def main():
                                     attachment.SaveAsFile(os.path.join(outputDir, attachment.FileName))
                                 except Exception as error:
                                     print("error when saving the attachment:" + str(error))
+                                    time.sleep(10)
+                                else:
+                                    print("Successfully saved file!\n")
+                                    time.sleep(2)
                             else:
                                 continue
-                        else:
-                            outOfRange+=1
-                            if outOfRange > 10:
-                                print(f"We are looking too far into the email inbox, I am going to exit us now \n")
-                                break
-                            continue
                     except Exception as error:
                         print("error when processing emails messages:" + str(error))
+                        time.sleep(10)
             message = messages.GetNext()
     except Exception as error:
         print("error when trying to open up emails" + str(error))
+        time.sleep(10)
+
+    print("All done, your files found should be placed in the directory you specified. Thank you!")
+    time.sleep(5)
 
 if __name__ == "__main__":
     main()
