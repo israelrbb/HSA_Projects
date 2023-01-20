2ac28ee4df3e195515f8cf62d9bd4dab0e8530b0
israelrbb <92654141+israelrbb@users.noreply.github.com>
2023-01-20 15:55:59 -0800

    Update HSAsystemInfo.py
    Changed the oder, to gather info at the start to speed up loading times.

--- itToolProject/HSAsystemInfo.py
+++ itToolProject/HSAsystemInfo.py
@@ -19,9 +19,30 @@ def resource_path(relative_path):
         base_path = sys._MEIPASS
     except Exception:
         base_path = os.path.abspath(".")
-
     return os.path.join(base_path, relative_path)
+    
 def main():
+    ########################################
+    # Gathering System Data
+    ########################################
+    # HostName
+    myhostname = socket.gethostname() # Get host name of PC
+    # IP
+    interface_list = netifaces.interfaces() # Get a list of interface identifiers connected to our machine
+    addresses = (netifaces.ifaddresses(iface) for iface in interface_list) # Get address of each interface
+    ipv4_address = (address[netifaces.AF_INET] for address in addresses if netifaces.AF_INET in address) # Check each address to make sure it is an IPV4 type
+    ipv4_addresses = [address[0]['addr'] for address in ipv4_address] 
+    myip = ipv4_addresses[0]  # Array in index 0 seems to be holding vpn IPV4 so set myip to that index
+    # Serial
+    myserial = os.popen("""wmic bios get SerialNumber """).read()                               # Make a call into command prompt
+    myserial = myserial.partition(' ')[2]    
+    myserial = myserial.strip()
+    # OS install 
+    myoginstall = os.popen("""systeminfo | find /i "original" """).read()                          # Get OS install
+    myoginstall = myoginstall.partition("Date:")[2]
+    myoginstall = myoginstall.split(",")                                      
+    myoginstall = myoginstall[0].strip()  
+
     ########################################
     # Main frame
     ########################################
@@ -39,11 +60,9 @@ def main():
     canvas.create_image(8, 8, image=tk_image, anchor="nw")
     #icon
     window.iconbitmap(resource_path("Santa_Cruz_County_Seal.ico"))
-    
     window.title("HSA-IT My PC Info")
     window.resizable(False, False) # Block full screen mode for it can cause our UI to look funky
     canvas.pack()    # Load our canvas into gui
-    
     fontvar1 = 'Helvetica 9 bold'
     fontvar2 = 'Helvetica 10 bold'
 
@@ -52,10 +71,8 @@ def main():
     ########################################
     hostname_frame = tk.LabelFrame(window, text="System Hostname:", background="white",font=(fontvar1))# Creating a white frame inside canvas with label
     hostname_frame.place(relwidth=0.3, relheight=0.2, relx=0.15,rely=0.25)                # Defining size/centering and loading into canvas
-    myhostname = socket.gethostname()                                                   # Get host name of PC
     button = tk.Button(hostname_frame, text=f"{myhostname}", font= (fontvar2), command=lambda: onclick(myhostname))   # Button to easliy copy text into clipboard
     button.pack()
-
     ######################
    
     ########################################
@@ -64,15 +81,6 @@ def main():
     ip_frame = tk.LabelFrame(window, text="System IPv4:", background="white", font=(fontvar1))      # Creating a white frame inside canvas with label
     ip_frame.place(relwidth=0.3, relheight=0.2, relx=0.15,rely=0.50)               # Defining size/centering and loading into canvas
     #myip = socket.gethostbyname(myhostname)                                        # Get IP address of PC
-    interface_list = netifaces.interfaces() # Get a list of interface identifiers connected to our machine
-    print(interface_list) #can delete prints if needed
-    addresses = (netifaces.ifaddresses(iface) for iface in interface_list) # Get address of each interface
-    ipv4_address = (address[netifaces.AF_INET] for address in addresses if netifaces.AF_INET in address) # Check each address to make sure it is an IPV4 type
-    print(ipv4_address)
-    ipv4_addresses = [address[0]['addr'] for address in ipv4_address]
-    print(ipv4_addresses)
-    
-    myip = ipv4_addresses[0]  # Array in index 0 seems to be holding vpn IPV4 so set myip to that index
     button2 = tk.Button(ip_frame, text=f"{myip}", font =(fontvar2), command=lambda: onclick(myip))        # Button to easliy copy text into clipboard
     button2.pack()
 
@@ -81,25 +89,16 @@ def main():
     ########################################
     serial_frame = tk.LabelFrame(window, text="Device Serial:", background="white",font=(fontvar1))     # Creating a white frame inside canvas with label
     serial_frame.place(relwidth=0.3, relheight=0.2, relx=0.55,rely=0.25)                          # Defining size/centering and loading into canvas
-    myserial = os.popen("""wmic bios get SerialNumber """).read()                               # Make a call into command prompt
-    myserial = myserial.partition(' ')[2]    
-    myserial = myserial.strip()
     button3 = tk.Button(serial_frame, text= f"{myserial}", font= (fontvar2), command=lambda: onclick(myserial), anchor="e")  # Button to easliy copy text into clipboard
     button3.pack()
     
-
     ########################################
     # Bottom right box
     ########################################
     install_frame = tk.LabelFrame(window, text="OS Install Date:", background="white",font=(fontvar1))             # Creating a white frame inside canvas with label
-    install_frame.place(relwidth=0.3, relheight=0.2, relx=0.55,rely=0.50)                           # Defining size/centering and loading into canvas
-    myoginstall = os.popen("""systeminfo | find /i "original" """).read()                          # Get OS install
-    myoginstall = myoginstall.partition("Date:")[2]
-    myoginstall = myoginstall.split(",")                                      
-    myoginstall = myoginstall[0].strip()                                                              # Deleteing white spaces or new lines
+    install_frame.place(relwidth=0.3, relheight=0.2, relx=0.55,rely=0.50)                           # Defining size/centering and loading into canvas                                                            # Deleteing white spaces or new lines
     button4 = tk.Button(install_frame, text=f"{myoginstall}", font=( fontvar2 ), command=lambda: onclick(myoginstall))             # Button to easliy copy text into clipboard
     button4.pack()
-
 
     ########################################
     # Run the script // OR on close
